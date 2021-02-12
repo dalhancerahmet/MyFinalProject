0d7011a5dd61ba02096e8ff296ea94cbf7fb3b4f
ahmet dalhançer <ahmed44dalhancer@hotmail.com>
2021-02-12 21:53:51 +0300

    Add project files.

---
+++ Entities/Abstract/IEntity.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Entities.Abstract
+{
+    //IEntity'i implement eden classlar bir nesnedir.
+    public interface IEntity
+    {
+    }
+}
