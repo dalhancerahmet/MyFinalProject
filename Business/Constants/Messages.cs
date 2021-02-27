5660c5cb93753769b824a40f1aa05de86c470813
ahmet dalhançer <ahmed44dalhancer@hotmail.com>
2021-02-27 15:10:12 +0300

    25Şubat

--- Business/Constants/Messages.cs
+++ Business/Constants/Messages.cs
@@ -12,6 +12,8 @@ namespace Business.Constants
         public static string ProductNameInvalid = "ürün ismi geçersiz";
         public static string MaintenanceTime = "Bakımdayız";
         public static string ProductsListed = "Ürünler listelendi";
-
+        public static string ProductCountOfCategoryError = "Bir kategoride en fazla 10 ürün olabilir";
+        public static string ProductNameAlreadyExits = "Aynı isimde birden fazla ürün olamaz";
+        public static string CategoryLimitExceded="Kategori Limiti aşıldı";
     }
 }
