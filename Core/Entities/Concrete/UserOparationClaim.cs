4880d034b2ece6b25bced568dc9beb557f3d11f4
ahmet dalhançer <ahmed44dalhancer@hotmail.com>
2021-02-28 19:30:37 +0300

    27 şubat 1.Parça

---
+++ Core/Entities/Concrete/UserOparationClaim.cs
@@ -0,0 +1,9 @@
+﻿namespace Core.Entities.Concrete
+{
+    public class UserOparationClaim : IEntity
+    {
+        public int Id { get; set; }
+        public int UserId { get; set; }
+        public int OparationClaimId { get; set; }
+    }
+}
